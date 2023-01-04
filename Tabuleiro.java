6fa2bd3751ebf4933e825002a5ad00615c097d2b
YuriTobias <118993772+YuriTobias@users.noreply.github.com>
2023-01-04 14:52:59 -0300

    Adicionei o comentário na linha 4

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,6 +1,7 @@
 public class Tabuleiro {
     private Setor[][] setores;
-
+    
+    // Ao inves de receber uma matriz como parametro, receber o numero de linhas e colunas da matriz
     public Tabuleiro(Setor[][] setores) {
         this.setores = setores;
     }
