5c03ca1a572f5a09b4f17e3a05fc70558bc576ef
mateus-mos <mateus.silvaoliveira13@outlook.com>
2023-01-05 11:04:47 -0300

    [ADD] Funcoes basicas para o gerarPortas

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -1,5 +1,9 @@
+import java.util.Random;
+
 public class Tabuleiro {
     private Setor[][] setores;
+    int linhas;
+    int colunas;
 
     public Tabuleiro(Setor[][] setores) {
         this.setores = setores;
@@ -14,7 +18,52 @@ public class Tabuleiro {
     }
 
     private void gerarPortas() {
+    }
 
+    private void acharVirus(Coordenada coord, Coordenada coord_antiga, Boolean virus_achado){
+        if(virus_achado == true)
+            return;
+        if(!coordenadaEhValida(coord))
+            return;
+        if(setores[coord.getX()][coord.getY()].isFonte()){
+            virus_achado = true;
+            return;
+        }
+
+        Coordenada nova_coord = coord;
+
+        /* (x+1, y) */
+        nova_coord.setX(nova_coord.getX() + 1);
+        if(nova_coord != coord_antiga){
+            /* Abrir portas de nova_coord para a coord_antiga */
+            acharVirus(nova_coord, coord, virus_achado);
+        }
+
+        /* (x-1, y) */
+        nova_coord.setX(nova_coord.getX() -2);
+        if(nova_coord != coord_antiga)
+            acharVirus(nova_coord, coord, virus_achado);
+
+        nova_coord.setX(nova_coord.getX() + 1);
+
+        /* (x, y+1) */
+        nova_coord.setY(nova_coord.getY() + 1);
+        if(nova_coord != coord_antiga)
+            acharVirus(nova_coord, coord, virus_achado);
+
+        /* (x, y-1) */
+        nova_coord.setY(nova_coord.getY() - 2);
+        if(nova_coord != coord_antiga)
+            acharVirus(nova_coord, coord, virus_achado);
+    }
+
+    private Boolean coordenadaEhValida(Coordenada coord){
+        if(coord.getX() >= linhas || coord.getX() < 0)
+            return false;
+        if(coord.getY() >= colunas || coord.getY() < 0)
+            return false;
+
+        return true;
     }
 
     public Setor buscaSetor(int x, int y) {
