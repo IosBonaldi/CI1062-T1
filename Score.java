7543d9d69f253f3606142a91b133241c9b306ace
I. Bonaldi <iosbonaldi@gmail.com>
2023-01-27 11:42:48 -0300

    [FIX] Corrigidas questões de encapsulamento intra e extra-classe

--- Score.java
+++ Score.java
@@ -1,13 +1,13 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Score implements Comparable<Score>{
+public class Score implements Comparable<Score> {
   Integer score;
   LocalDateTime date;
 
   public Score(Integer score, LocalDateTime date) {
-    this.score = score;
-    this.date = date;
+    this.setScore(score);
+    this.setDate(date);
   }
 
   public Integer getScore() {
@@ -28,15 +28,16 @@ public class Score implements Comparable<Score>{
 
   @Override
   public int compareTo(Score arg0) {
-    if(this.score > arg0.score) return 1;
-    if(this.score < arg0.score) return -1;
+    if (this.getScore() > arg0.getScore())
+      return 1;
+    if (this.getScore() < arg0.getScore())
+      return -1;
     return 0;
   }
 
-  public void printScore(){
+  public void printScore() {
     DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-    String s = "Score: " + this.score;
-    System.out.println(String.format("%-15s", s) + " Date: " + this.date.format(dtFormatter));
+    String s = "Score: " + this.getScore();
+    System.out.println(String.format("%-15s", s) + " Date: " + this.getDate().format(dtFormatter));
   }
 }
-
