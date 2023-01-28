d36f77af723904e9415ec41dc438f8c6b97b76c7
Matheus Souza <matheus-oliveira17@hotmail.com>
2023-01-28 03:44:52 -0300

    Atualização da estrutura de dados utilizada para armazenar scores e modificação do método de printar scores.

--- LogHandler.java
+++ LogHandler.java
@@ -5,36 +5,48 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.time.format.DateTimeFormatter;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class LogHandler {
-  String filePath;
-  ArrayList<Score> scores;
+  private String filePath;
+  private TreeSet<Score> scores;
 
   public LogHandler(String filePath) {
-    this.filePath = filePath;
-    this.scores = new ArrayList<>();
+    this.setFilePath(filePath);
+    this.setScores(new TreeSet<Score>());
   }
 
   public String getFilePath() {
     return filePath;
   }
 
-  public ArrayList<Score> getScores() {
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
+
+  public void setScores(TreeSet<Score> scores) {
+    this.scores = scores;
+  }
+
+  public TreeSet<Score> getScores() {
     return scores;
   }
 
-  // Verifica existência do arquivo de log;
-  public boolean logFileExist(){
+  /**
+   * Verifica existência do arquivo de log.
+   */
+  public boolean logFileExist() {
     File file = new File(this.filePath);
     return file.exists();
   }
 
-  // Caso o arquivo de log não exista, cria um novo;
-  public void createLogFile(){
-    if(!logFileExist()){
+  /**
+   * Cria um novo arquivo em caso de não existência.
+   */
+  public void createLogFile() {
+    if (!logFileExist()) {
       File file = new File(this.filePath);
       try {
         file.createNewFile();
@@ -48,8 +60,12 @@ public class LogHandler {
     }
   }
 
-  // Atualiza arquivo de log com score e data de finalização da partida;
-  public void logFileManipulation(Integer score){
+  /**
+   * Atualiza arquivo de log com score e data de finalização da partida.
+   * 
+   * @param score
+   */
+  public void logFileManipulation(Integer score) {
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(this.filePath, true))) {
       bw.write("Score: " + score);
       bw.newLine();
@@ -61,40 +77,49 @@ public class LogHandler {
     }
   }
 
-  // Recupera os 10 maiores scores do arquivo de log (caso houver);
-  public void topTenScores(){
-    try (BufferedReader br = new BufferedReader(new FileReader(this.filePath))) {
+  /**
+   * Recupera os dez maiores scores do arquivo de log.
+   */
+  public void recoverTopTenScores() {
+    try (BufferedReader br = new BufferedReader(new FileReader(this.getFilePath()))) {
       String line;
       Integer score = null;
       LocalDateTime date = null;
 
-      // Looping para buscar os valores de score e data no arquivo;
-      while((line = br.readLine()) != null){
-        if(line.contains("Score:")){
+      while ((line = br.readLine()) != null) {
+        if (line.contains("Score:")) {
           score = Integer.valueOf(line.split(": ")[1]);
           continue;
         }
 
-        if(line.contains("Date:")){
+        if (line.contains("Date:")) {
           date = LocalDateTime.parse(line.split(": ")[1]);
-          if(score != null && date != null){
+          if (score != null && date != null) {
             this.scores.add(new Score(score, date));
             score = null;
             date = null;
           }
         }
       }
-
-      // Ordenação decrescente;
-      Collections.sort(this.scores, Collections.reverseOrder());
-
-      // Busca dos 10 maiores scores (caso houver);
-      for (int i = 0; i < this.scores.size(); i++){
-        if(i > 9) break;
-        this.scores.get(i).printScore();
-      }
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  /**
+   * Função que retorna uma string contendo as dez maiores pontuações
+   * ou todas pontuações as caso a árvore possua dez ou menos registros.
+   */
+  public String topTenToString() {
+    recoverTopTenScores();
+    DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy, HH:mm:ss");
+
+    String str = this.scores.stream()
+        .limit(10)
+        .map(e -> String.format("%-15s", "Score: " + e.getScore()) + " Date: " +
+            e.getDate().format(dtFormatter))
+        .collect(Collectors.joining("\n"));
+
+    return str;
   }
 }
