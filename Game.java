a49b4d282091947665be81ac4acfdd9a3e904b5d
mateus-mos <mateus.silvaoliveira13@outlook.com>
2023-02-13 17:56:13 -0300

    [FIX] inimigos atacando mesmo estando mortos(Zumbis???)

--- Game.java
+++ Game.java
@@ -27,23 +27,28 @@ public class Game {
                     turn = Turn.ENEMIES1;
                     break;
                 case ENEMIES1:
-                    System.out.println("-----------------------------");
-                    System.out.println("|      P1 enemy turn        |");
-                    System.out.println("-----------------------------");
-                    for (Enemy e : p1.getSection().getEnemies()) {
-                        e.attack(p1);
+                    if(p1.getSection().countEnemiesAlive() != 0) {
+
+                        System.out.println("-----------------------------");
+                        System.out.println("|      P1 enemy turn        |");
+                        System.out.println("-----------------------------");
+                        for (Enemy e : p1.getSection().getEnemies()) {
+                            e.attack(p1);
+                        }
                     }
                     turn = Turn.ENEMIES2;
                     break;
                 case ENEMIES2:
-                    System.out.println("-----------------------------");
-                    System.out.println("|      P2 enemy turn        |");
-                    System.out.println("-----------------------------");
-                    for (Enemy e : p2.getSection().getEnemies()) {
-                        e.attack(p2);
+                    if(p1.getSection().countEnemiesAlive() != 0) {
+                        System.out.println("-----------------------------");
+                        System.out.println("|      P2 enemy turn        |");
+                        System.out.println("-----------------------------");
+                        for (Enemy e : p2.getSection().getEnemies()) {
+                            e.attack(p2);
+                        }
+                        match.increaseCycles();
                     }
                     turn = Turn.PLAYER1;
-                    match.increaseCycles();
                     break;
                 default:
                     break;
